379a6cd10dc965489204ecc004367ac2905d39be
Seeker4264 <sebashmorales@gmail.com>
2025-10-05 16:51:24 -0400

    commit inicial

---
+++ src/main/java/sistemas/operativos/proyecto1/Proyecto1.java
@@ -0,0 +1,13 @@
+package sistemas.operativos.proyecto1;
+
+/**
+ *
+ * @author Sebastián
+ * @author Nicole
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("\nHello World!\n");
+    }
+}

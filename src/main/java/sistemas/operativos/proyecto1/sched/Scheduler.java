5094b5c8527efd38809d4dce6c8704e62aefa0b4
Nicolepinto09 <nicole.pinto@correo.unimet.edu.ve>
2025-10-22 22:41:20 -0400

    Scheduler

---
+++ src/main/java/sistemas/operativos/proyecto1/sched/Scheduler.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemas.operativos.proyecto1.sched;
+
+/**
+ *
+ * @author nicolepinto
+ */
+public class Scheduler {
+    
+}
